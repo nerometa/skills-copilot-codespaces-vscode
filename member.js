d9e9562ff9db2d86d91cbd46c105a18d9661af24
nerometa <95143530+nerometa@users.noreply.github.com>
2024-10-14 10:02:08 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsHeader() {
+    var header = document.getElementById('header');
+    var h1 = document.createElement('h1');
+    var text = document.createTextNode('Skills');
+    h1.appendChild(text);
+    header.appendChild(h1);
+}
