fd14f6f5f53134e0746bc7f1ebe1b000ef67d441
nerometa <95143530+nerometa@users.noreply.github.com>
2024-10-14 10:12:34 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,27 @@
-function skillsHeader() {
-  return "Skills";
+function skillsMember() {
+  return {
+    title: 'Skills',
+    skills: [
+      {
+        name: 'JavaScript',
+        level: '90%',
+      },
+      {
+        name: 'React',
+        level: '85%',
+      },
+      {
+        name: 'HTML & (S)CSS',
+        level: '80%',
+      },
+      {
+        name: 'Node.js',
+        level: '70%',
+      },
+      {
+        name: 'MongoDB',
+        level: '60%',
+      },
+    ],
+  };
 }
